06a0f86dc17e4d19789f8fcd067d7cc3a98f50e8
xiaoqianghou <2662651103@qq.com>
2017-01-31 22:37:08 +0800

    Initial Commit

---
+++ Github/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Github
+//
+//  Created by WuQiang on 2017/1/31.
+//  Copyright © 2017年 WuQiang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Github/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Github
+//
+//  Created by WuQiang on 2017/1/31.
+//  Copyright © 2017年 WuQiang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
